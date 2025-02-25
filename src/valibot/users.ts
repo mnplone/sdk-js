212d1a5d8c28ed221ce0fff564ce9a1c6457d615
otsuchka <dev@otsu.me>
2025-02-25 11:09:37 +0300

    Added twitch and discord properties for User schema

--- src/valibot/users.ts
+++ src/valibot/users.ts
@@ -84,6 +84,12 @@ export const valiObjectUserSchema = pipe(
 		social_vk: optional(
 			number(),
 		),
+		social_discord: optional(
+			string(),
+		),
+		social_twitch: optional(
+			string(),
+		),
 		games: optional(
 			number(),
 		),
