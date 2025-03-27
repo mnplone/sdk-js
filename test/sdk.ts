d6242ac8425eeee002ad4be61b976bde8a355cd6
otsuchka <dev@otsu.me>
2025-03-27 17:32:22 +0300

    Added tests for gchat.get method

--- test/sdk.ts
+++ test/sdk.ts
@@ -1,8 +1,3 @@
 import { M1 } from '../src/m1.js';
-import { refresh_hook } from '../src/hooks/refresh.js';
 
-export const sdk = new M1({
-	hooks: {
-		1: refresh_hook,
-	},
-});
+export const sdk = new M1({});
