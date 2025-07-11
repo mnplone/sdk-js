bb4033157d406938b00dcd224bafa7190bc30c65
Daniil Kirichenko <daniil@kirick.me>
2025-07-11 13:10:20 +0200

    Update tests

--- src/api/users.test.ts
+++ src/api/users.test.ts
@@ -5,6 +5,8 @@ import {
 } from 'vitest';
 import { sdk } from '../../test/sdk.js';
 
+const BOT_USER_ID = Number.parseInt(process.env.TEST_BOT_USER_ID ?? '112426');
+
 describe('users.get', () => {
 	describe('single user', () => {
 		test('user', async () => {
@@ -15,7 +17,7 @@ describe('users.get', () => {
 		});
 
 		test('bot', async () => {
-			const response = await sdk.users.get(5);
+			const response = await sdk.users.get(BOT_USER_ID);
 
 			expect(response.data.bot).toStrictEqual({
 				owner_user_id: 1,
