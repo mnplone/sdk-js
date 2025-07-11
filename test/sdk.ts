cacc2f7f0439eeb1bce7963d6f0f5fe95e569d58
Daniil Kirichenko <daniil@kirick.me>
2025-07-11 14:28:50 +0200

    Remove unused code

--- test/sdk.ts
+++ test/sdk.ts
@@ -4,9 +4,3 @@ export const sdk = new M1({
 	hostname: process.env.TEST_HOSTNAME,
 	access_token: process.env.TEST_ACCESS_TOKEN,
 });
-
-// sdk.im.send({
-// 	user_id: 1,
-// 	image_token: 'wQxWr',
-// 	send_id: '1234567890',
-// });
