a5992a4406e5ba19555908baacd05f58a74f3a71
otsuchka <dev@otsu.me>
2025-10-24 03:17:37 +0300

    Updated dev.ts exports

--- src/dev.ts
+++ src/dev.ts
@@ -5,6 +5,7 @@ export { M1ApiData } from './api/data.js';
 export { M1ApiFriends } from './api/friends.js';
 export { M1ApiGchat } from './api/gchat.js';
 export { M1ApiIm } from './api/im.js';
+export { M1ApiInventory } from './api/inventory.js';
 export { M1ApiOauth } from './api/oauth.js';
 export { M1ApiTrades } from './api/trades.js';
 export { M1ApiUsers } from './api/users.js';
@@ -12,12 +13,15 @@ export { M1ApiUsers } from './api/users.js';
 export {
 	type CallMethodOptions,
 	type CallMethodResponse,
+	type RequestOptions,
 } from './m1.js';
+
 export * from './valibot/auth.js';
 export * from './valibot/data.js';
 export * from './valibot/friends.js';
 export * from './valibot/gchat.js';
 export * from './valibot/im.js';
+export * from './valibot/inventory.js';
 export * from './valibot/items.js';
 export * from './valibot/trades.js';
 export * from './valibot/users.js';
