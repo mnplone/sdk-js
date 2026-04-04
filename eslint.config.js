03a6b5724e8de1f7ed9a6eeee53605829bcc6b44
otsuchka <dev@otsu.me>
2026-04-04 10:33:02 +0300

    updated kirick/lint version

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import { configCommon } from '@kirick/lint/eslint/common';
 import { configNode } from '@kirick/lint/eslint/node';
+import { configOxlint } from '@kirick/lint/eslint/oxlint';
 import { defineConfig } from 'eslint/config';
 
-export default defineConfig([...configCommon, ...configNode]);
+export default defineConfig([...configCommon, ...configNode, ...configOxlint]);
